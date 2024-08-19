7af216f32064b17ab93411f88e718717b3722126
dlswns2480 <dlswns661035@gmail.com>
2024-08-19 15:14:14 +0900

    feat : Test Container 설정

--- api/src/test/kotlin/com/hana/aiforkid/ApiApplicationTests.kt
+++ api/src/test/kotlin/com/hana/aiforkid/ApiApplicationTests.kt
@@ -1,9 +1,14 @@
 package com.hana.aiforkid
 
+import com.hana.aiforkid.support.TestContainerSupport
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = [TestContainerSupport::class])
 class ApiApplicationTests {
 
     @Test
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,11 +5,17 @@ plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("plugin.jpa") version "1.9.24"
+    `java-test-fixtures`
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testFixturesImplementation("org.springframework.boot:spring-boot-starter-test")
+    testFixturesImplementation("org.testcontainers:testcontainers:1.19.7")
+    testFixturesImplementation("org.testcontainers:mysql:1.19.7")
 }
 
 tasks {
